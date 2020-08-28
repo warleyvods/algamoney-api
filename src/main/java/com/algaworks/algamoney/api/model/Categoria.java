6189e02e3a2d6aa9a70cc17756444262e848243f
Warley Vinicius <warleyvods@gmail.com>
2020-08-27 22:19:28 -0300

    Ajutes

--- src/main/java/com/algaworks/algamoney/api/model/Categoria.java
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -2,9 +2,7 @@ package com.algaworks.algamoney.api.model;
 
 import com.algaworks.algamoney.api.arquiteturabase.entidade.Entidade;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -15,12 +13,13 @@ import javax.validation.constraints.Size;
 @Data
 @Entity
 @Table(name = "categoria")
+@EqualsAndHashCode(callSuper = true)
 public class Categoria extends Entidade {
 
 	@NotNull
 	@Size(min = 3, max = 20)
 	private String nome;
 
-
-
 }
+
+
